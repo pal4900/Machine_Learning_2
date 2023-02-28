b4a35e98cbab429a564ef9d28265cf4c230f6a0b
Pankaj <ppal4900@gmail.com>
2023-02-28 16:04:27 +0530

    logger and excption

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from sales.logger import logging
+from sales.exception import SalesException
 
 app = Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
+    logging.info("Logger") 
     return 'Starting Machine Learning Project'
 
 
