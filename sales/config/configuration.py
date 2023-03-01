f01b67dfffeed04edeff4e3b4fbe410b4bfab03f
Pankaj <ppal4900@gmail.com>
2023-03-01 23:34:32 +0530

    Data Ingestion

--- sales/config/configuration.py
+++ sales/config/configuration.py
@@ -24,8 +24,40 @@ class configuration:
         except Exception as e:
             raise SalesException(e,sys) from e 
 
-    def get_data_ingestion_config() -> DataIngestionconfig: 
-        pass
+    def get_data_ingestion_config(self) -> DataIngestionconfig: 
+        try:
+            artifact_dir = self.training_pipeline_config.artifact_dir
+            data_ingestion_artifact_dir=os.path.join(
+                artifact_dir,
+                DATA_INGESTION_ARTIFACT_DIR,
+                self.time_stamp
+            )
+            data_ingestion_info=self.config_info[DATA_INGESTION_CONFIG_KEY]
+
+            dataset_download_url = data_ingestion_info[DATA_INGESTION_DOWNLOAD_URL_KEY]
+            tgz_download_dir = os.path.join(
+            data_ingestion_artifact_dir,
+            data_ingestion_info[DATA_INGESTION_TGZ_DOWNLOAD_DIR_KEY])
+
+            raw_data_dir = os.path.join(data_ingestion_artifact_dir,
+            data_ingestion_info[DATA_INGESTION_RAW_DATA_DIR_KEY]
+            )
+
+            ingested_data_dir = os.path.join(
+                data_ingestion_artifact_dir,
+                data_ingestion_info[DATA_INGESTION_INGESTED_DIR_NAME_KEY]
+            )
+            ingested_train_dir = os.path.join(
+                ingested_data_dir,
+                data_ingestion_info[DATA_INGESTION_TRAIN_DIR_KEY]
+            )
+            ingested_test_dir =os.path.join(
+                ingested_data_dir,
+                data_ingestion_info[DATA_INGESTION_TEST_DIR_KEY]
+            )
+
+        except Exception as e:
+               raise SalesException(e,sys) from e 
 
     def get_data_validation_config() -> DataValidationconfig:
         pass
